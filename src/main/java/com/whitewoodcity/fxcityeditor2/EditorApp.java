821cef2ba4859704258a9e988018843f01a54fb7
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-09-02 23:24:06 +0800

    menu bar

--- src/main/java/com/whitewoodcity/fxcityeditor2/EditorApp.java
+++ src/main/java/com/whitewoodcity/fxcityeditor2/EditorApp.java
@@ -1,13 +1,25 @@
 package com.whitewoodcity.fxcityeditor2;
 
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import module javafx.controls;
 
 public class EditorApp extends javafx.application.Application {
   @Override
   public void start(Stage stage) throws Exception {
-    stage.setScene(new Scene(new Button("test")));
+
+    var menuBar = new MenuBar();
+    var menu = new Menu("File");
+    var save = new MenuItem("Save");
+    var load = new MenuItem("Load");
+    menu.getItems().addAll(save, load);
+    menuBar.getMenus().add(menu);
+
+    var gamePane = GameApp.embeddedLaunch(new GameApp());
+    gamePane.setRenderFill(Color.TRANSPARENT);
+
+    var vbox = new VBox();
+    vbox.getChildren().addAll(menuBar, gamePane);
+
+    stage.setScene(new Scene(vbox));
     stage.show();
   }
 }
---
+++ src/main/java/com/whitewoodcity/fxcityeditor2/GameApp.java
@@ -0,0 +1,11 @@
+package com.whitewoodcity.fxcityeditor2;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class GameApp extends GameApplication {
+  @Override
+  protected void initSettings(GameSettings settings) {
+
+  }
+}
