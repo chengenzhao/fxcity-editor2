3e1bb1dcc1b0500a67126f35a06bcebb2c775ab0
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-09-06 21:51:46 +0800

    bump fxcity to 0.6.19

--- src/main/java/com/whitewoodcity/fxcityeditor2/GameApp.java
+++ src/main/java/com/whitewoodcity/fxcityeditor2/GameApp.java
@@ -1,11 +1,29 @@
 package com.whitewoodcity.fxcityeditor2;
 
-import com.almasb.fxgl.app.GameApplication;
-import com.almasb.fxgl.app.GameSettings;
+import module com.almasb.fxgl.all;
 
 public class GameApp extends GameApplication {
+
+  Entity entity;
+
   @Override
   protected void initSettings(GameSettings settings) {
 
+  }
+
+  @Override
+  protected void initUI() {
+    FXGL.getGameScene().setCursor(Cursor.DEFAULT);
+  }
+
+  @Override
+  protected void initGame() {
+    entity = new Entity();
+    FXGL.getGameWorld().addEntities(entity);
+    var redDot = new Circle(3);
+    redDot.setFill(Color.RED);
+    entity.getViewComponent().addDevChild(redDot);
+    entity.setX(200);
+    entity.setY(200);
   }
 }
