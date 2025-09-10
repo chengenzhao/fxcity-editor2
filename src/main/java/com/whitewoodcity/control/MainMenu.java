1181dababcf64fa386ad4e90932a41e4d921e0d7
chengenzhao <chengen.zhao@mail.mcgill.ca>
2025-09-10 19:27:45 +0800

    refactor

--- src/main/java/com/whitewoodcity/control/MainMenu.java
+++ src/main/java/com/whitewoodcity/control/MainMenu.java
@@ -4,7 +4,6 @@ import module javafx.controls;
 import com.almasb.fxgl.dsl.FXGL;
 import com.whitewoodcity.fxcityeditor.GameApp;
 import com.whitewoodcity.fxgl.vectorview.JVG;
-import javafx.scene.shape.Rectangle;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -29,35 +28,7 @@ public class MainMenu extends MenuBar {
         try {
           var jsonString = Files.readString(Paths.get(file.getPath()));
           var jvg = new JVG(jsonString);
-          jvg.trim();
-          jvg.setMouseTransparent(true);
-          FXGL.<GameApp>getAppCast().entity.getViewComponent().addChild(jvg);
-          var d = jvg.getDimension();
-          var rect = new Rectangle(d.getWidth(), d.getHeight());
-          rect.getStrokeDashArray().addAll(5d);
-          rect.setFill(Color.TRANSPARENT);
-          rect.setStroke(Color.web("#039ED3"));
-          var xy = jvg.getXY();
-          rect.setX(xy.getX());
-          rect.setY(xy.getY());
-          FXGL.<GameApp>getAppCast().entity.getViewComponent().addChild(rect);
-          rect.setOnMousePressed(e -> {
-            var x = rect.getX();
-            var y = rect.getY();
-            var ex = e.getX();
-            var ey = e.getY();
-            ChangeListener<Number> c = (_, _, _) -> jvg.set(rect.getX(), rect.getY());
-            rect.xProperty().addListener(c);
-            rect.yProperty().addListener(c);
-
-            rect.setOnMouseDragged(ee -> {
-              var dx = ee.getX() - ex;
-              var dy = ee.getY() - ey;
-              rect.setX(x + dx);
-              rect.setY(y + dy);
-            });
-          });
-
+          FXGL.<GameApp>getAppCast().addNode(jvg);
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
--- src/main/java/com/whitewoodcity/fxcityeditor/GameApp.java
+++ src/main/java/com/whitewoodcity/fxcityeditor/GameApp.java
@@ -1,6 +1,10 @@
 package com.whitewoodcity.fxcityeditor;
 
 import module com.almasb.fxgl.all;
+import module com.whitewoodcity.fxcity;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
 
 public class GameApp extends GameApplication {
 
@@ -29,5 +33,63 @@ public class GameApp extends GameApplication {
     entity.getViewComponent().addDevChild(redDot);
     entity.setX(200);
     entity.setY(200);
+  }
+
+  public void addNode(Node node) {
+    switch (node) {
+      case JVG jvg -> jvg.trim();
+      default -> {
+      }
+    }
+    node.setMouseTransparent(true);
+    entity.getViewComponent().addChild(node);
+    var rect = new Rectangle();
+    switch (node) {
+      case JVG jvg -> {
+        var d = jvg.getDimension();
+        rect.setWidth(d.getWidth());
+        rect.setHeight(d.getHeight());
+
+        var xy = jvg.getXY();
+        rect.setX(xy.getX());
+        rect.setY(xy.getY());
+      }
+      default -> {
+      }
+    }
+
+    rect.getStrokeDashArray().addAll(5d);
+    rect.setFill(Color.TRANSPARENT);
+    rect.setStroke(Color.web("#039ED3"));
+
+    entity.getViewComponent().addDevChild(rect);
+
+    ChangeListener<Number> c = (_, _, _) -> {
+      switch (node) {
+        case JVG jvg -> jvg.set(rect.getX(), rect.getY());
+        case ImageView imageView -> {
+          imageView.setX(rect.getX());
+          imageView.setY(rect.getY());
+        }
+        default -> {
+        }
+      }
+    };
+    rect.xProperty().addListener(c);
+    rect.yProperty().addListener(c);
+
+    rect.setOnMousePressed(e -> {
+      var x = rect.getX();
+      var y = rect.getY();
+      var ex = e.getX();
+      var ey = e.getY();
+
+      rect.setOnMouseDragged(ee -> {
+        var dx = ee.getX() - ex;
+        var dy = ee.getY() - ey;
+        rect.setX(x + dx);
+        rect.setY(y + dy);
+      });
+    });
   }
 }
